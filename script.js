6cf33db1d13bad6bfe40ecb58ea9c306ad2b57f9
Ferch <chaconfernando3110@gmail.com>
2026-01-09 12:24:24 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function aceptar() {
+    document.getElementById('modal').style.display = 'flex';
+}
+
+function cerrarModal() {
+    location.href = "https://wa.me/+50237923681"; // Puedes poner tu link de WhatsApp
+}
